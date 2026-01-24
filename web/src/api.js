b87fb009ec797f352ab3a1f21e8980c276cbdd00
GyanPrakashkushwaha <gyanp7880@gmail.com>
2026-01-24 18:21:20 +0530

    add .

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,8 @@
+const BASE_URL = "https://internops-2.onrender.com"
 
 export async function fetchHistory(){
     try{
-        const res = await fetch("https://internops-2.onrender.com/web/dashboard/history")
+        const res = await fetch(`${BASE_URL}/web/dashboard/history`)
         const data = await res.json()
         if(data.status == "success"){
             return data.data.history
@@ -14,7 +15,7 @@ export async function fetchHistory(){
 
 export async function fetchAnalysisHistory() {
     try{
-        const res = await fetch("https://internops-2.onrender.com/web/analysis/history")
+        const res = await fetch(`${BASE_URL}/web/analysis/history`)
         const data = await res.json()
         if(data.status == "success"){
             return data.data.history
@@ -27,13 +28,13 @@ export async function fetchAnalysisHistory() {
 
 export async function fetchAnalysisReport(id) {
     try{
-        const res = await fetch(`https://internops-2.onrender.com/web/analysis/report/${id}`)
+        const res = await fetch(`${BASE_URL}/web/analysis/report/${id}`)
         const data = await res.json()
         if (data.status == "success"){
             return data.report
         }
         return {}
-    }catch{
-
-    } 
+    }catch(err){
+        console.log(err)
+    }
 }
